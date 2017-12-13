5a6cabc98a232974b0c1d25bf344c0f2c48d8f57
xiele <870161348@qq.com>
2017-12-13 10:47:31 +0800

    添加测试

--- 22/22.5/jacobi/jacobi_test.go
+++ 22/22.5/jacobi/jacobi_test.go
@@ -6,19 +6,24 @@ import (
 
 // TestJacobi1 ...
 func TestJacobi1(t *testing.T) {
-	if i, e := Jacobi(5, 3593); i != -1 || e != nil { //try a unit test on function
-		t.Error("测试没通过")
-	} else {
-		t.Log("通过了") //记录一些你期望记录的信息
+	if onet(5, 3593, -1, t) &&
+		onet(5, 3889, 1, t) &&
+		onet(14, 137, 1, t) &&
+		onet(299, 397, -1, t) &&
+		onet(37603, 48611, 1, t) &&
+		onet(85, 101, 1, t) &&
+		onet(101, 1987, 1, t) &&
+		onet(29, 541, -1, t) &&
+		onet(31706, 43789, -1, t) &&
+		onet(55, 179, -1, t) {
+		t.Log("PASS")
 	}
 }
 
-// Jacobi(5, 3889) // 1
-// Jacobi(14, 137) // 1
-// Jacobi(55, 179) // -1
-// Jacobi(299, 397) // -1
-// Jacobi(37603, 48611) // 1
-// Jacobi(85, 101) // 1
-// Jacobi(29, 541) // -1
-// Jacobi(101, 1987) // 1
-// Jacobi(31706, 43789) // -1
+func onet(up int64, down uint64, res int8, t *testing.T) bool {
+	if i, e := Jacobi(up, down); i != res || e != nil {
+		t.Errorf("Jacobi(%d %d), Expected %d,got %d", up, down, res, i)
+		return false
+	}
+	return true
+}
