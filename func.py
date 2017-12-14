0d7dd1b1891608f53a8784caabb903ba221b00da
xiele <870161348@qq.com>
2017-12-14 15:59:56 +0800

    重构因式分解

--- func.py
+++ func.py
@@ -1,14 +1,10 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
-import commands
+import sympy
 
 #因式分解
 def factor(number):
-    (status, output) = commands.getstatusoutput('factor ' + str(number))
-    if not status == 0: 
-        print output
-        return False
-    return output.split(':')[1].split(' ')[1:]
+    return sympy.factorint(number, multiple=True)
 
 #判断一个数是否是素数
 def is_prime(number):
@@ -50,11 +46,11 @@ def quadraticResidues(num):
 #数论概论第19张习题--->19.6答案
 if __name__ == "__main__":
     # print aModM(2, 294408, 294409)
-    print (1758249*1758249)%118901521
-    print aModM(45274074, 1, 118901521)
-    print aModM(45274074, 2, 118901521)
-    print aModM(45274074, 4, 118901521)
-    print aModM(45274074, 8, 118901521)
+    # print (1758249*1758249)%118901521
+    # print aModM(45274074, 1, 118901521)
+    # print aModM(45274074, 2, 118901521)
+    # print aModM(45274074, 4, 118901521)
+    # print aModM(45274074, 8, 118901521)
     # print is_prime(285707540662569884530199015485751094149)
     # print is_prime(285 707 540 662 569 884 530 199 015 485 751 094 149)
 
