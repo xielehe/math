00c55138b5024aa443271ccc3238aaff7a6ae73a
xiele <870161348@qq.com>
2017-12-22 19:34:13 +0800

    添加golang 判断素数

--- lib/mymath/mymath.go
+++ lib/mymath/mymath.go
@@ -20,3 +20,19 @@ func Floor(x float64) int {
 	}
 	return int(x)
 }
+
+//IsPrime ...
+func IsPrime(value int) bool {
+	if value <= 3 {
+		return value >= 2
+	}
+	if value%2 == 0 || value%3 == 0 {
+		return false
+	}
+	for i := 5; i*i <= value; i += 6 {
+		if value%i == 0 || value%(i+2) == 0 {
+			return false
+		}
+	}
+	return true
+}
