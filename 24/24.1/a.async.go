a7d0d66ad2de39270d6e62d20656068be17faf67
xiele <870161348@qq.com>
2017-12-23 19:15:55 +0800

    异步实现

---
+++ 24/24.1/a.async.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sync"
+	"time"
+
+	"../../lib/mymath"
+)
+
+func a2abb2(p int) (int, int, bool) {
+	n := int(math.Sqrt(float64(p))) + 2
+	for i := 1; i < n; i++ {
+		for j := 1; j < n; j++ {
+			if i*i+i*j+j*j == p {
+				return i, j, true
+			}
+		}
+	}
+	return 0, 0, false
+}
+
+type a2ABB2 struct {
+	a int
+	b int
+	p int
+}
+
+func a(N int) []a2ABB2 {
+	result := []a2ABB2{}
+	r := make(chan a2ABB2, N)
+	wg := sync.WaitGroup{}
+	for n := 0; n < N+1; n++ {
+		wg.Add(1)
+		go func(x int, r chan a2ABB2) {
+			isHas := false
+			if mymath.IsPrime(x) {
+				a, b, has := a2abb2(x)
+				if has {
+					r <- a2ABB2{a: a, b: b, p: x}
+					wg.Done()
+					isHas = true
+				}
+			}
+			if !isHas {
+				wg.Done()
+			}
+		}(n, r)
+	}
+	wg.Wait()
+	close(r)
+	for i := range r {
+		result = append(result, i)
+	}
+	return result
+}
+
+func testa(t []a2ABB2) bool {
+	for _, i := range t {
+		if i.p != 3 && i.p%3 != 1 {
+			return false
+		}
+	}
+	return true
+}
+
+//异步代码
+func main() {
+	// fmt.Println(a(50)) //[{1 1 3} {1 2 7} {1 3 13} {2 3 19} {1 5 31} {3 4 37} {1 6 43}]
+	// 通过观察发现，满足要求的素数都满足模3余1(除了3),下面通过更多的素数测试,以便增加猜测正确的概率
+	start := time.Now()
+	limit := 1000000
+	container := a(limit)
+	isMod3r1 := testa(container)
+	fmt.Println(isMod3r1)
+	end := time.Now()
+	fmt.Printf("LongCalculation took this amount of time: %s\n", end.Sub(start))
+	// 通过测试，发现<1000000的结果都满足猜想.
+}
